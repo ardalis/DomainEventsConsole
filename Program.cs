7287902bd513fa4de1880937f7b94aa8be23593b
Steve Smith <steve@kentsmiths.com>
2021-04-23 14:39:45 -0400

    initial version

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DomainEventsConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Load services");
+            var services = ConfigureServices();
+
+            var app = services
+                        .BuildServiceProvider()
+                        .GetRequiredService<App>();
+
+            app.Run();
+
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddTransient<App>();
+
+            return services;
+        }
+    }
+
+    public class App
+    {
+        public void Run()
+        {
+            Console.WriteLine("App running.");
+        }
+    }
+}
